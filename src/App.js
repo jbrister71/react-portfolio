492d427ef436d92f0d897927eae11110ac563c18
Bell <jacobbrister71@gmail.com>
2022-10-27 23:52:40 -0400

    added About Me component with text

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import AboutMe from './components/AboutMe';
 
 function App() {
 
@@ -17,7 +18,7 @@ function App() {
   return (
     <div className="App">
       <Header navCategories={navCategories} currentCategory={currentCategory} setCurrentCategory={setCurrentCategory}></Header>
-      {currentCategory === navCategories[0] ? <h1>About me</h1> : <></>}
+      {currentCategory === navCategories[0] ? <AboutMe></AboutMe> : <></>}
       {currentCategory === navCategories[1] ? <h1>Porfolio</h1> : <></>}
       {currentCategory === navCategories[2] ? <h1>Contact</h1> : <></>}
       {currentCategory === navCategories[3] ? <h1>Resume</h1> : <></>}
