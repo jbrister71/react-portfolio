4234d8a1e913dd81cfca11281c3b2e73ed57cf06
Bell <jacobbrister71@gmail.com>
2022-10-28 02:02:02 -0400

    updated forms layout

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
+
 function Contact() {
     return (
         <div>
             <h2 className='mt-4 section-name'>Contact</h2>
+            <form id='contact-form'>
+                <div className='d-flex flex-column form-div mt-5 ms-5 ps-5'>
+                    <label htmlFor='name'>Name: </label>
+                    <input className='text-input' type='text' name='name' defaultValue='Name'></input>
+                </div>
+                <div className='d-flex flex-column form-div mt-2 ms-5 ps-5'>
+                    <label htmlFor='email'>Email: </label>
+                    <input className='text-input' type='email' name='email' defaultValue='Email'></input>
+                </div>
+                <div className='d-flex flex-column form-div mt-2 ms-5 ps-5'>
+                    <label htmlFor='message'>Message: </label>
+                    <textarea className='message-text' rows='5' defaultValue='Message'></textarea>
+                </div>
+            </form>
         </div>
     )
 }
